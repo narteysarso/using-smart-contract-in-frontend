4a9ad9a74c156865601081a6a1b2b30fc992004e
narteysarso <narteysarso@gmail.com>
2022-06-25 22:10:31 +0000

    add frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,50 @@
-import { ethers } from "ethers";
+import { ethers, utils } from "ethers";
 import { useEffect, useState } from "react";
 
 import "./App.css";
 import {
     abi,
     CONTRACT_ADDRESS,
+    DEFAULT_FORM_VALUES,
     DEFAULT_PRODUCT_IMAGE,
     EVENTS,
 } from "./constants";
 
-//get contract instance
-const getcontract = () => {
+//get wrapped provider from MetaMask.
+//this allows use to query the blockchain
+const getProvider = () => {
     if (!window.ethereum) {
         throw new Error("Install MetaMask browser extension");
     }
 
+    //provider allows us to only read from or query the blockchain
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
+    return provider;
+};
+
+//connect to MetaMask wallet
+const connectWallet = async () => {
+    //we also use the provider to issue commands to the MetaMask wallet
+    const provider = getProvider();
+
+    //requesting for permission to use MetaMask wallets/accounts
+    //without this all our transactions will fail.
+    const accounts = await provider.send("eth_requestAccounts", []);
+
+    //we will use the first account in our MetaMask wallet
+    return accounts[0];
+};
+
+//get contract instance
+const getcontract = () => {
+    const provider = getProvider();
+
+    //while provider allows us to only read from or query the blockchain
+    //signers allows us to query and also write to the blockchain.
     const signer = provider.getSigner();
 
+    //we need to use signers on our contract instance to be able to send create product transactions
     const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
 
     return contract;
@@ -60,6 +86,255 @@ const getProducts = async (contract) => {
     return await Promise.all(products);
 };
 
+//add product
+function CreateProduct() {
+    //tracking uploading a new product
+    const [loading, setLoading] = useState(false);
+
+    //binds to the form field data
+    const [formData, setFormData] = useState(DEFAULT_FORM_VALUES);
+
+    //handle changes to form inputs
+    const handleFormFieldChange = (event) => {
+        setFormData((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    //handles form submition
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            setLoading(true);
+
+            const { sku, name, image, description, price, quantityAvailable } =
+                formData;
+
+            if (!name || !price || !sku || !quantityAvailable) {
+                alert("Required data must be provided");
+                return;
+            }
+
+            //format price into Ethers
+            const priceInEth = utils.parseEther(price);
+
+            //convert quantityAvailable and sku into int
+            //NB: they can also be converted to Bignumber
+            const iQuantity = parseInt(quantityAvailable);
+            const iSku = parseInt(sku);
+
+            const contract = getcontract();
+
+            const tnx = await contract.createProduct(
+                iSku,
+                name,
+                image,
+                description,
+                priceInEth,
+                iQuantity
+            );
+
+            await tnx.wait();
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <>
+            <div
+                className="modal fade"
+                id="productModal"
+                tabIndex="-1"
+                aria-labelledby="productModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="productModalLabel">
+                                New Product
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                                disabled={loading}
+                            ></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="sku"
+                                        className="col-form-label"
+                                    >
+                                        SKU *
+                                    </label>
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        id="sku"
+                                        name="sku"
+                                        value={formData["sku"]}
+                                        min={1}
+                                        required={true}
+                                        onChange={handleFormFieldChange}
+                                        disabled={loading}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="name"
+                                        className="col-form-label"
+                                    >
+                                        Name *
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="name"
+                                        name="name"
+                                        value={formData["name"]}
+                                        required={true}
+                                        onChange={handleFormFieldChange}
+                                        disabled={loading}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="image"
+                                        className="col-form-label"
+                                    >
+                                        Image Url
+                                    </label>
+                                    <input
+                                        type="url"
+                                        className="form-control"
+                                        id="image"
+                                        value={formData["image"]}
+                                        onChange={handleFormFieldChange}
+                                        disabled={loading}
+                                        name="image"
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="price"
+                                        className="col-form-label"
+                                    >
+                                        Price *
+                                    </label>
+                                    <input
+                                        name="price"
+                                        type="number"
+                                        value={formData["price"]}
+                                        min={0.001}
+                                        step={0.001}
+                                        required={true}
+                                        className="form-control"
+                                        id="price"
+                                        onChange={handleFormFieldChange}
+                                        disabled={loading}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="quantityAvailable"
+                                        className="col-form-label"
+                                    >
+                                        Quantity Available *
+                                    </label>
+                                    <input
+                                        name="quantityAvailable"
+                                        type="number"
+                                        value={formData["quantityAvailable"]}
+                                        min={1}
+                                        required={true}
+                                        className="form-control"
+                                        id="quantityAvailable"
+                                        onChange={handleFormFieldChange}
+                                        disabled={loading}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="description"
+                                        className="col-form-label"
+                                    >
+                                        Description:
+                                    </label>
+                                    <textarea
+                                        name="description"
+                                        className="form-control"
+                                        id="description"
+                                        value={formData["description"]}
+                                        onChange={handleFormFieldChange}
+                                        disabled={loading}
+                                    ></textarea>
+                                </div>
+                                <div className="mb-3 d-flex justify-content-between">
+                                    <button
+                                        type="reset"
+                                        className="btn btn-outline-danger"
+                                        disabled={loading}
+                                    >
+                                        Reset
+                                    </button>
+                                    <button
+                                        type="submit"
+                                        className="btn btn-success"
+                                        disabled={loading}
+                                    >
+                                        Create &nbsp;
+                                        {loading && (
+                                            <div
+                                                className="spinner-border text-light"
+                                                role="status"
+                                            >
+                                                <span className="visually-hidden">
+                                                    Loading...
+                                                </span>
+                                            </div>
+                                        )}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button
+                type="button"
+                data-bs-toggle="modal"
+                className="btn btn-outline-success"
+                data-bs-target="#productModal"
+                disabled={loading}
+            >
+                Create Product
+            </button>
+        </>
+    );
+}
+
+//navigation bar
+function TopNavbar({ defaultAccount }) {
+    return (
+        <nav className="navbar bg-light">
+            <div className="container">
+                <b className="navbar-brand">Products DApp</b>
+                {defaultAccount && <CreateProduct />}
+            </div>
+        </nav>
+    );
+}
+
 //generate product card
 function Card({ productInfo, ...props }) {
     const [loading, setLoading] = useState();
@@ -100,14 +375,18 @@ function Card({ productInfo, ...props }) {
             ></img>
 
             <div className="card-body">
-                <span>SKU: #{sku.toString()}</span>
+                <span>SKU: #{sku?.toString()}</span>
                 <h5 className="card-title">{name}</h5>
                 <p className="card-text">{description}</p>
-                <p className="card-text">Price: {price.toString()} Eth</p>
+                <p className="card-text">
+                    Price: {utils.formatEther(price)} Eth
+                </p>
                 <p className="card-text">
                     Available: {quantityAvailable?.toString()}
                 </p>
-                <p className="card-text">Sold: {quantitySold?.toString()}</p>
+                <p className="card-text">
+                    Sold: {quantitySold?.toString() || 0}
+                </p>
                 <button
                     className="btn btn-primary d-flex align-items-center"
                     onClick={buyProduct}
@@ -130,10 +409,20 @@ function Card({ productInfo, ...props }) {
 
 function App() {
     //tracks products
-    const [products, setProducts] = useState({});
+    const [products, setProducts] = useState([]);
 
     //tracks loading activity
     const [loading, setLoading] = useState(false);
+
+    //tracks the default account to be used
+    const [defaultAccount, setDefaultAccount] = useState(null);
+
+    //connect to MetaMask on first render
+    useEffect(() => {
+        connectWallet()
+            .then((account) => setDefaultAccount(account))
+            .catch(console.log);
+    }, []);
 
     //fetch products once when the component is rendered
     useEffect(() => {
@@ -156,32 +445,29 @@ function App() {
         //note that JS spread (...) operator is used to collect all arguments
         //that are passed to the callback function as eventData
         contract.on(EVENTS.PRODUCT_CREATED, (...eventData) => {
-            const {
+            const [
                 index,
                 sku,
                 name,
                 image,
                 description,
-                price,
                 quantityAvailable,
-            } = eventData;
-
-            //used as key to find product in `products`
-            const skuString = sku?.toString();
+                price,
+            ] = eventData;
 
             setProducts((prevProducts) => {
-                return {
-                    ...prevProducts,
-                    [skuString]: {
-                        index,
-                        sku,
-                        name,
-                        image,
-                        description,
-                        price,
-                        quantityAvailable,
-                    },
+                const newProduct = {
+                    index: index.toNumber(),
+                    sku,
+                    name,
+                    image,
+                    description,
+                    price,
+                    quantityAvailable,
                 };
+
+                //append newProduct
+                return [...prevProducts, newProduct];
             });
         });
 
@@ -190,30 +476,36 @@ function App() {
         //that are passed to the callback function as eventData
         contract.on(EVENTS.PRODUCT_SOLD, (...eventData) => {
             //get product info from event data
-            const {
-                sku,
-                quantitySold: totalQuantitySold,
-                quantityAvailable: newQuantityAvailable,
-            } = eventData;
+            const [index,,, totalQuantitySold, newQuantityAvailable] = eventData;
 
             //used as key to identify product in `products`
-            const skuString = sku?.toString();
+            const idx = index?.toNumber();
 
             setProducts((prevProducts) => {
-                //find product
-                const oldProductDetails = prevProducts[skuString];
+                //find product by index
+                const oldProductDetails = prevProducts[idx];
+
+                if (!oldProductDetails) {
+                    return {
+                        ...prevProducts,
+                    };
+                }
 
                 //generate updated product info to replace old product info
                 const updatedProductDetails = {
                     ...oldProductDetails,
-                    totalQuantitySold,
-                    newQuantityAvailable,
+                    quantitySold: totalQuantitySold,
+                    quantityAvailable: newQuantityAvailable,
                 };
 
-                return {
-                    ...prevProducts,
-                    [skuString]: updatedProductDetails,
-                };
+                //insert to replace old product details
+                const products = [
+                    ...prevProducts.slice(0, idx),
+                    updatedProductDetails,
+                    ...prevProducts.slice(idx + 1),
+                ];
+
+                return products;
             });
         });
 
@@ -229,11 +521,11 @@ function App() {
         return <h1>Loading...</h1>;
     }
 
-    console.log(products);
     return (
         <>
+            <TopNavbar defaultAccount={defaultAccount} />
             <div className="container">
-                <div className="row">
+                <div className="row py-3">
                     {/* product listings */}
                     {Object.values(products)?.map((product, idx) => (
                         <Card key={idx} productInfo={product} />
